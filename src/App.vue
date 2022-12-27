b3aefbe5a11f48e26ba885dcaf89664a575e72ad
moydulhaque <95543844+moydulhaque@users.noreply.github.com>
2022-12-27 21:06:29 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
+  <div class="container mx-auto">
+  <div class="mt-10">
     <h2 class="font-bold text-red-500">{{ msg }}</h2>
-    <button @click="test">Click Me</button>
   </div>
+  <div class="mt-10">
+    <button class="border p-2" @click="test">Click Me</button>
+  </div>
+  <div class="mt-10">
+    <h2 class="mb-4 text-2xl ">List of Courses</h2>
+    <ul>
+      <li v-for="course in courses" class="shadow p-4 mb-4 border border-gray-200">
+        {{ course.name }} - {{ course.instructor }}
+      </li>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
@@ -10,12 +22,17 @@ export default {
   name:'App',
   data(){
     return {
-      msg: 'Welcome to Vue.js'
+      msg: 'Welcome to Vue.js',
+courses: [
+      { name: 'Vue.js', instructor: 'Polash Mahmud' },
+      { name: 'Laravel', instructor: 'Rasel Ahmed' },
+      { name: 'HTML', instructor: 'John Doe' },
+    ]
     }
   },
   methods: {
     test() {
-      console.log('test')
+      this.msg = 'Welcome Message Changed'
     }
   }
 }
